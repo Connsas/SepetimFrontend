fdcf07cb1a62336b0b1f491662da16aff3a5c577
İsmail <ismailoner1403@gmail.com>
2024-01-08 17:03:45 +0300

    Ürün sayfası için geliştirmeler

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ProductsComponent } from './components/products/products.component';
+import { ProductPageComponent } from './components/product-page/product-page.component';
 
 export const routes: Routes = [
     {path:"",pathMatch:"full", component:ProductsComponent},
     {path:"products", component:ProductsComponent},
     {path:"products/category/:categoryId", component:ProductsComponent},
+    {path:"products/:productName", component:ProductPageComponent}
 ];
