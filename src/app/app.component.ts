407d3d8f2b7d7900b1ef06289a8c3b5f362ee6a0
İsmail <ismailoner1403@gmail.com>
2024-01-14 19:17:16 +0300

    Footer eklenmesi ve navinin sticky yapılması

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { ProductsComponent } from './components/products/products.component';
 import { CategoryComponent } from './components/category/category.component';
 import { FormsModule } from '@angular/forms';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormsModule, NaviComponent, ProductsComponent, CategoryComponent, HomepageComponent],
+  imports: [CommonModule, RouterOutlet, FormsModule, NaviComponent, ProductsComponent, CategoryComponent, HomepageComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
